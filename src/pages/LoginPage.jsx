1c1fb29ec962d277e6f6a3befb270e76707b0107
Bheny <Bernard.tay@gmail.com>
2023-06-17 18:27:48 +0000

    changed the login route

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -45,7 +45,7 @@ const LoginPage = () => {
           <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter your password" />
         </div>
         <div className="flex items-center justify-between">
-          <Link to="/dashboard/student"> <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          <Link to="/dashboard"> <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
             Sign In
           </button></Link>
           <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
